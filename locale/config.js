84ad0423fe894cb99841ad6e55cba67c0eec1d2d
CNMathon <wsnbtkk@163.com>
2018-12-10 17:08:48 +0800

    More rationalized translation

--- locale/config.js
+++ locale/config.js
@@ -1,73 +1,42 @@
 moment.locale('zh', {
   months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
-  monthsShort: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
+  monthsShort: '1 月_2月_3 月_4 月_5 月_6 月_7 月_8 月_9 月_10 月_11 月_12 月'.split('_'),
   weekdays: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
   weekdaysShort: '周日_周一_周二_周三_周四_周五_周六'.split('_'),
   weekdaysMin: '日_一_二_三_四_五_六'.split('_'),
   longDateFormat: {
-    LT: 'Ah点mm分',
-    LTS: 'Ah点m分s秒',
-    L: 'YYYY-MM-DD',
-    LL: 'YYYY年MMMD日',
-    LLL: 'YYYY年MMMD日Ah点mm分',
-    LLLL: 'YYYY年MMMD日 ddd H:mm:ss',
-    l: 'YYYY-MM-DD',
-    ll: 'YYYY年MMMD日',
-    lll: 'YYYY年MMMD日Ah点mm分',
-    llll: 'YYYY年MMMD日ddddAh点mm分'
-  },
-  meridiemParse: /凌晨|早上|上午|中午|下午|晚上/,
-  meridiemHour: function (hour, meridiem) {
-    if (hour === 12) {
-      hour = 0;
-    }
-    if (meridiem === '凌晨' || meridiem === '早上' ||
-        meridiem === '上午') {
-      return hour;
-    } else if (meridiem === '下午' || meridiem === '晚上') {
-      return hour + 12;
-    } else {
-      // '中午'
-      return hour >= 11 ? hour : hour + 12;
-    }
-  },
-  meridiem: function (hour, minute, isLower) {
-    var hm = hour * 100 + minute;
-    if (hm < 600) {
-      return '凌晨';
-    } else if (hm < 900) {
-      return '早上';
-    } else if (hm < 1130) {
-      return '上午';
-    } else if (hm < 1230) {
-      return '中午';
-    } else if (hm < 1800) {
-      return '下午';
-    } else {
-      return '晚上';
-    }
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'M 月 D 日',
+    LL: 'YYYY 年 M 月 DD 日',
+    LLL: 'YYYY-MM-DD HH:mm',
+    LLLL: 'YYYY-MM-DD HH:mm:ss',
+    l: 'M/D',
+    ll: 'YY/M/D',
+    lll: 'YYYY-MM-DD HH:mm',
+    llll: 'YYYY-MM-DD HH:mm:ss'
   },
   calendar: {
     sameDay: function () {
-      return this.minutes() === 0 ? '[今天]Ah[点整]' : '[今天]LT';
+      return this.minutes() === 0 ? '[今天] h [时整]' : '[今天] LT';
     },
     nextDay: function () {
-      return this.minutes() === 0 ? '[明天]Ah[点整]' : '[明天]LT';
+      return this.minutes() === 0 ? '[明天] h [时整]' : '[明天] LT';
     },
     lastDay: function () {
-      return this.minutes() === 0 ? '[昨天]Ah[点整]' : '[昨天]LT';
+      return this.minutes() === 0 ? '[昨天] h [时整]' : '[昨天] LT';
     },
     nextWeek: function () {
       var startOfWeek, prefix;
       startOfWeek = moment().startOf('week');
       prefix = this.unix() - startOfWeek.unix() >= 7 * 24 * 3600 ? '[下]' : '[本]';
-      return this.minutes() === 0 ? prefix + 'dddAh点整' : prefix + 'dddAh点mm';
+      return this.minutes() === 0 ? prefix + 'ddd h 时整' : prefix + 'ddd h 时 mm';
     },
     lastWeek: function () {
       var startOfWeek, prefix;
       startOfWeek = moment().startOf('week');
       prefix = this.unix() < startOfWeek.unix() ? '[上]' : '[本]';
-      return this.minutes() === 0 ? prefix + 'dddAh点整' : prefix + 'dddAh点mm';
+      return this.minutes() === 0 ? prefix + 'ddd h 时整' : prefix + 'ddd h 时 mm';
     },
     sameElse: 'LL'
   },
@@ -104,7 +73,7 @@ moment.locale('zh', {
   },
   week: {
     // GB/T 7408-1994《数据元和交换格式·信息交换·日期和时间表示法》与ISO 8601:1988等效
-    dow: 1, // Monday is the first day of the week.
+    dow: 0, // Sunday is the first day of the week.
     doy: 4  // The week that contains Jan 4th is the first week of the year.
   }
 });
